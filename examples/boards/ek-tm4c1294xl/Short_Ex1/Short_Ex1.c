161d63de65e8282808bd756bc7e98a773819d83a
AeRgIo74 <aergio74@gmail.com>
2024-09-01 15:11:55 -0400

    ej corto uno finish

--- examples/boards/ek-tm4c1294xl/Short_Ex1/Short_Ex1.c
+++ examples/boards/ek-tm4c1294xl/Short_Ex1/Short_Ex1.c
@@ -1,27 +1,3 @@
-//*****************************************************************************
-//
-// blinky.c - Simple example to blink the on-board LED.
-//
-// Copyright (c) 2013-2020 Texas Instruments Incorporated.  All rights reserved.
-// Software License Agreement
-// 
-// Texas Instruments (TI) is supplying this software for use solely and
-// exclusively on TI's microcontroller products. The software is owned by
-// TI and/or its suppliers, and is protected under applicable copyright
-// laws. You may not combine this software with "viral" open-source
-// software in order to form a larger program.
-// 
-// THIS SOFTWARE IS PROVIDED "AS IS" AND WITH ALL FAULTS.
-// NO WARRANTIES, WHETHER EXPRESS, IMPLIED OR STATUTORY, INCLUDING, BUT
-// NOT LIMITED TO, IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-// A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE. TI SHALL NOT, UNDER ANY
-// CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL, OR CONSEQUENTIAL
-// DAMAGES, FOR ANY REASON WHATSOEVER.
-// 
-// This is part of revision 2.2.0.295 of the EK-TM4C1294XL Firmware Package.
-//
-//*****************************************************************************
-
 #include <stdint.h>
 #include <stdbool.h>
 #include "inc/hw_memmap.h"
@@ -64,6 +40,7 @@ main(void)
 
     //
     // Enable the GPIO port that is used for the on-board LED.
+    //ON para los puertos N y OF para los puertos F
     //
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPION);
 
@@ -78,7 +55,7 @@ main(void)
     // Enable the GPIO pin for the LED (PN0).  Set the direction as output, and
     // enable the GPIO pin for digital function.
     //
-    GPIOPinTypeGPIOOutput(GPIO_PORTN_BASE, GPIO_PIN_0);
+    GPIOPinTypeGPIOOutput(GPIO_PORTN_BASE, GPIO_PIN_1 | GPIO_PIN_0);
 
     //
     // Loop forever.
@@ -88,24 +65,30 @@ main(void)
         //
         // Turn on the LED.
         //
+        GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_1, 0);
         GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_0, GPIO_PIN_0);
-
         //
         // Delay for a bit.
         //
-        for(ui32Loop = 0; ui32Loop < 200000; ui32Loop++)
+        for(ui32Loop = 0; ui32Loop < 1000000; ui32Loop++)
         {
         }
 
         //
         // Turn off the LED.
         //
-        GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_0, 0x0);
+        GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_1, GPIO_PIN_1);
+        GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_0, GPIO_PIN_0);
 
         //
         // Delay for a bit.
         //
-        for(ui32Loop = 0; ui32Loop < 200000; ui32Loop++)
+        for(ui32Loop = 0; ui32Loop < 1000000; ui32Loop++)
+        {
+        }
+        GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_1, 0);
+        GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_0, 0);
+        for(ui32Loop = 0; ui32Loop < 1000000; ui32Loop++)
         {
         }
     }
